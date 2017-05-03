5d2b6a1aac576c159757b3267463abd4ede8f112
Eva Maria <emgaitan@stratio.com>
2017-05-03 17:25:10 +0200

    [EGE-166]: Add changes to example

--- src/app/+components/switch/switch.component.ts
+++ src/app/+components/switch/switch.component.ts
@@ -15,38 +15,28 @@ export class SwitchComponent {
    public form: FormGroup;
    public disabled: boolean;
 
-   public apiDoc: ApiDoc = {
-      title: 'Switch',
-      description: 'PENDING',
-      haveModel: false,
-      apiSection: {
-         inputs: [],
-         outputs: []
-      },
-      exampleDesc: ''
-   };
-
    constructor() {
       this.loading = true;
       this.apiDoc = {
          title: 'Switch',
-         description: 'PENDING.',
-         haveModel: true,
+         description: 'Component designed to enable or disable whatever you need. Switch component can be used in a form or any part of your web.',
+         haveModel: false,
          apiSection: {
             inputs: [
-               { paramName: 'PENDING', type: TYPES.STR, required: true, details: 'PENDING' }
+               { paramName: 'stModel', type: TYPES.BOOL, required: true, details: 'This boolean changes when user interacts with switch' },
+               { paramName: 'qaTag', type: TYPES.STR, required: true, details: 'Id value for qa test.' },
+               { paramName: 'label', type: TYPES.STR, required: false, details: 'This boolean changes when user interacts with switch' },
+               { paramName: 'labelPosition', type: TYPES.STR, required: false, details: 'This string indicates the label position. It can be left, right or top. By default, label is positioned at top' },
+               { paramName: 'contextualHelp', type: TYPES.STR, required: false, details: 'Tooltip displayed when user clicks on the help button' }
             ],
             outputs: []
          },
-         exampleDesc: `PENDING`
+         exampleDesc: `Next, you can see how to work a switch with different variations`
       };
 
       this.form = new FormGroup({
-         requiredSwitch: new FormControl({ value: this.model, disabled: false }, Validators.required),
+         switch: new FormControl({ value: this.model, disabled: false }, Validators.required),
       });
    }
 
-   onSubmitForm(): void {
-      console.log(this.form);
-   }
 }
