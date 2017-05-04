13e3d3242ef25b3abb872d03f526c4e51916b256
Eva Maria <emgaitan@stratio.com>
2017-05-04 15:32:17 +0200

    [EGE-166]: Fix error

--- src/app/+components/switch/switch.component.ts
+++ src/app/+components/switch/switch.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 import { ApiDoc, TYPES } from 'shared';
 import { FormGroup, Validators, FormControl } from "@angular/forms";
 
@@ -14,7 +14,8 @@ export class SwitchComponent {
    public model = false;
    public form: FormGroup;
    public disabled: boolean;
-
+   public apiDoc: ApiDoc;
+   
    constructor() {
       this.loading = true;
       this.apiDoc = {
@@ -23,11 +24,31 @@ export class SwitchComponent {
          haveModel: false,
          apiSection: {
             inputs: [
-               { paramName: 'stModel', type: TYPES.BOOL, required: true, details: 'This boolean changes when user interacts with switch' },
+               {
+                  paramName: 'stModel',
+                  type: TYPES.BOOL,
+                  required: true,
+                  details: 'This boolean changes when user interacts with switch'
+               },
                { paramName: 'qaTag', type: TYPES.STR, required: true, details: 'Id value for qa test.' },
-               { paramName: 'label', type: TYPES.STR, required: false, details: 'This boolean changes when user interacts with switch' },
-               { paramName: 'labelPosition', type: TYPES.STR, required: false, details: 'This string indicates the label position. It can be left, right or top. By default, label is positioned at top' },
-               { paramName: 'contextualHelp', type: TYPES.STR, required: false, details: 'Tooltip displayed when user clicks on the help button' }
+               {
+                  paramName: 'label',
+                  type: TYPES.STR,
+                  required: false,
+                  details: 'This boolean changes when user interacts with switch'
+               },
+               {
+                  paramName: 'labelPosition',
+                  type: TYPES.STR,
+                  required: false,
+                  details: 'This string indicates the label position. It can be left, right or top. By default, label is positioned at top'
+               },
+               {
+                  paramName: 'contextualHelp',
+                  type: TYPES.STR,
+                  required: false,
+                  details: 'Tooltip displayed when user clicks on the help button'
+               }
             ],
             outputs: []
          },
